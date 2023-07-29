b4b40759f19f0d58dd304ca1d30508dba1369151
israt-mim <israt.mim3053@gmail.com>
2023-07-29 17:19:47 +0600

    index.css login

--- ssms/src/components/Login.jsx
+++ ssms/src/components/Login.jsx
@@ -3,13 +3,12 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const Login = ({setIsAdminLoggedIn , setIsStudentLoggedIn}) => {
+const Login = ({ setIsAdminLoggedIn, setIsStudentLoggedIn }) => {
   const [studentId, setstudentId] = useState("");
   const [password, setPassword] = useState("");
 
   // for admin login
   //const [loggedIn, setLoggedIn] = useState(false);
-
 
   const navigate = useNavigate();
 
@@ -18,51 +17,56 @@ const Login = ({setIsAdminLoggedIn , setIsStudentLoggedIn}) => {
     // need to add logic to handle login authentication here : admin logic has been added.
     // need to replace "adminid" and "adminpass" with the actual admin credentials
     if (studentId === "adminid" && password === "adminpass") {
-     // setLoggedIn(true);
-      setIsAdminLoggedIn(true) ;
+      // setLoggedIn(true);
+      setIsAdminLoggedIn(true);
       navigate("/adminDashboard");
-    }
-    else if (studentId == "20203038") {  //  studentId.trim() !== ''
+    } else if (studentId == "20203038") {
+      //  studentId.trim() !== ''
       // setLoggedIn(true) ;
-      setIsStudentLoggedIn(true) ;
+      setIsStudentLoggedIn(true);
 
-      navigate('/studentDashboard'); 
-      
-    }
-    else {
+      navigate("/studentDashboard");
+    } else {
       alert("Invalid credentials. Please try again.");
     }
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label> ID:</label>
-          <input
-            type="text"
-            value={studentId}
-            onChange={(e) => setstudentId(e.target.value)}
-            required
-          />
+    <div className="login-bg">
+      <div className="login-box">
+        <div className="login-logo">
+          <div className="logo"></div>
         </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+        <div className="login-box1">
+          <h2>Login Here</h2>
+          <form onSubmit={handleLogin}>
+            <div className="login-box2">
+              <p> User ID </p>
+              <input
+                type="text"
+                value={studentId}
+                onChange={(e) => setstudentId(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <p>Password </p>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <div className="submit-button">
+              <button type="submit">Login</button>
+              <p>Forget password?</p>
+            </div>
+          </form>
         </div>
-        <div>
-          <button type="submit">Login</button>
-        </div>
-      </form>
+      </div>
     </div>
   );
 };
 
 export default Login;
-
